ec11892195519c5fb44f3c28bd930f4bc3f67c52
9jana8 <9jana8@gmail.com>
2025-09-12 16:49:28 +0200

    add user authentication with Flask-Login and validations
    - Added password hashing with Bcrypt
    - Introduced SQLAlchemy db instance and User model
    - Integrated Flask-Login: login_user, logout_user, current_user, login_required
    - Added custom RegistrationForm validations (unique username and email checks)
    - Enabled user login and session handling
    - Restricted navigation options: show logout/account when logged in, login/register otherwise

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskapp.models import User
 
 class RegistrationForm(FlaskForm): 
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -8,6 +9,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user_exists = User.query.filter_by(username=username.data).first()
+        if user_exists:
+            raise ValidationError('This username is taken. Please choose unique one.')
+        
+    def validate_email(self, email):
+        user_exists = User.query.filter_by(email=email.data).first()
+        if user_exists:
+            raise ValidationError('Account was already created with this email address. Please log in.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
--- flaskapp/models/note.py
+++ flaskapp/models/note.py
@@ -1,5 +1,13 @@
-from app import db
 from datetime import datetime
+from flaskapp import db
+from flask_login import UserMixin
 
-class Note(db.Model):
-    __tablename__ = "notes"
+class Note(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
