ff539ff3cc4afd825fa047001bfc041df9c7728b
9jana8 <9jana8@gmail.com>
2025-09-18 13:54:14 +0200

    - created Config class - set secret variables as environmental variables

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,15 +1,13 @@
 '''__init__.py tells Python this is a package. Creates the Flask app + db.'''
 from flask import Flask
-import secrets
-import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flaskapp.config import Config
 
 # Initialize Flask app
 app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
