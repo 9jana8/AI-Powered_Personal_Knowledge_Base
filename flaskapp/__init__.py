d05f65ae4453bae4893e4614efddbf229428b9d9
9jana8 <9jana8@gmail.com>
2025-09-18 10:51:33 +0200

    initial commit

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -8,8 +8,8 @@ from flask_login import LoginManager
 
 # Initialize Flask app
 app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', secrets.token_hex(16))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
