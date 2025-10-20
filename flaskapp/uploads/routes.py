4caedf346c7f94d9996e10c02fdc8db29b8cfecf
9jana8 <9jana8@gmail.com>
2025-10-20 10:24:23 +0200

    fixed bugs introduced when upload_modal.html was included in the layout.html without defining conditions with it

--- flaskapp/uploads/routes.py
+++ flaskapp/uploads/routes.py
@@ -6,24 +6,30 @@ import os, secrets
 from flask_login import current_user, login_required
 from werkzeug.utils import secure_filename
 
+
 uploads = Blueprint('uploads', __name__)
 
-ALLOWED_EXTENSIONS = {'pdf', 'txt', 'md'}
+ALLOWED_EXTENSIONS = {'pdf', 'txt', 'md'}  # set (vs list, vs dict)
 
-def allowed_file(filename):
-    if ('.' in filename) and (filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS):
-        return True
-    else:
-        return False
+def allowed_file(filename: str) -> bool:
+    good_extension = filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    dot_in_name = '.' in filename
+    return good_extension and dot_in_name
+
 
 @uploads.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload_file():
     form = UploadForm()
+    print("Form submitted:", request.method, form.validate_on_submit())
     if form.validate_on_submit():
         file = form.file.data
-        
         filename = secure_filename(file.filename)
+
+        if not allowed_file(filename):
+            flash('Invalid file type. Allowed types: pdf, txt, md', 'danger')
+            return redirect(url_for('uploads.upload_file'))
+
         random_hex = secrets.token_hex(8)
         _, extension = os.path.splitext(filename)
         stored_name = random_hex + extension
@@ -35,12 +41,13 @@ def upload_file():
         doc = Document(
             filename=filename, 
             stored_name=stored_name, 
-            status='QUEUED',
-            user_id=current_user.id)
+            status='Uploaded',
+            user_id=current_user.id
+        )
         db.session.add(doc)
         db.session.commit()
 
-        flash('File uploaded and queued for processing', 'success')
+        flash('File uploaded successfully!', 'success')
         return redirect(url_for('uploads.upload_file'))
     
     documents = Document.query.filter_by(user_id=current_user.id).all()
@@ -60,16 +67,14 @@ def view_document(filename):
 @login_required
 def delete_document(doc_id):
     doc = Document.query.get_or_404(doc_id)
-    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], doc.filename)
+    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], doc.stored_name)
 
-    """Delete file from filepath"""
     if os.path.exists(filepath):
         os.remove(filepath)
 
-    """Delete record from the database"""
     db.session.delete(doc)
     db.session.commit()
     flash('File is deleted successfully', 'success')
-    return redirect(url_for(uploads.upload_file))
+    return redirect(url_for('uploads.upload_file'))
 
     
